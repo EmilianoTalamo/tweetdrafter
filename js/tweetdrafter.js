d2b63e865a8a998119b41a1868cb11c297ee150c
EmilianoTalamo <emilianotalamo@gmail.com>
2017-06-20 20:14:28 -0300

    initial commit

---
+++ js/tweetdrafter.js
@@ -0,0 +1,109 @@
+var tweet = $('#textarea');
+var chars = 140 - $(tweet).val().length;
+var tweetlist;
+var current = $('document');
+$('#charcount').html(chars);
+$('pageone').unbind('swiperight');
+function charcount()
+{
+	chars = 140 - tweet.val().length;
+	$('#charcount').html(chars);
+	if(chars < 0)
+	{
+		$('#charcount').css('color','red');
+	}
+	else
+	{
+		$('#charcount').css('color','black');
+	}
+}
+
+function refreshtweets()
+{
+	console.log("refreshing");
+	if(current.html() == "")
+	{
+		dilit();
+	}
+	$(tweetlist).unbind('tap');
+	$(tweetlist).unbind('taphold');
+	$('#backbtn').unbind('tap');
+	tweetlist = $('#tweetlist').children();
+	$(tweetlist).on('tap', function()
+	{
+		current = $(this);
+		edit();
+	});
+	$(tweetlist).on('taphold', function()
+	{
+		console.log("holding");
+		$(tweetlist).unbind('tap');
+		$('#actions').popup('open', {transition: 'fade', changeHash : false});
+		current = $(this);
+		$('#popuptweet').text($(current).text());
+		$('#ghosttweet').text($(current).text());
+		$('#ghosttweet').attr('value',$(current).text());
+		$('#tweetlink').attr('href','https://twitter.com/home?status=' + $(current).text());
+		refreshtweets();
+	});
+}
+
+function edit()
+{
+	console.log("editing");
+	tweet.val($(current).text());
+	charcount();
+	$.mobile.navigate('#pagetwo', {transition: 'fade'});
+	$(tweet).on('input', function()
+	{
+		$(current).text($(tweet).val());
+		if($('#charcount').text() < 0)
+		{
+			$(current).css('border-left','2px solid red');
+		}
+		else
+		{
+			$(current).css('border-left','2px solid #2196F3');
+		}
+	});
+}
+
+function newtw()
+{
+	console.log("new");
+	n = $('<li class="ui-li-static ui-body-inherit ui-first-child waves-effect waves-button  waves-effect waves-button"></li>');
+	$("#tweetlist").append(n);
+	current = n;
+	edit();
+}
+
+function dilit()
+{
+	console.log("deleting");
+	$(current).remove();
+}
+
+$(document).on("pagebeforeshow", "#pageone", refreshtweets);
+$(tweet).on('input', charcount);
+$('#newtweet').on('tap', newtw);
+$('#del').on('tap', function()
+{
+	$.mobile.back();
+	dilit();
+});
+
+$('#copybtn').on('tap', function()
+{
+	$.mobile.back();
+	$.mobile.toast({ message: 'Tweet copiado!' });
+});
+
+$('#contextualdelbtn').on('tap', function()
+{
+	$.mobile.back();
+	dilit();
+	$.mobile.toast({ message: 'Tweet eliminado!' });
+	refreshtweets();
+});
+
+var clipboard = new Clipboard('#copybtn');
